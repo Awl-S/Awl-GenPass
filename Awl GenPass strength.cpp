6bd7f099afbda3977c8d8962df1c7a97d58a6aea
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2021-06-17 21:20:11 +0300

    Update Awl GenPass strength.cpp

--- Awl GenPass strength.cpp
+++ Awl GenPass strength.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Batnikov Nikita <designAwl@bk.ru>
+ * Copyright (c) 2021 Nikita Batnikov <designAwl@bk.ru>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above

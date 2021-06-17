ce1b9ff50131b05734d4c047361b13e366546063
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2021-06-17 21:21:28 +0300

    Update Awl GenPass.cpp

--- Awl GenPass.cpp
+++ Awl GenPass.cpp
@@ -1,5 +1,5 @@
-﻿/*
- * Copyright (c) 2021 Batnikov Nikita <designAwl@bk.ru>
+/*
+ * Copyright (c) 2021 Nikita Batnikov <designAwl@bk.ru>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -20,63 +20,63 @@
 #include <ctime> 
 #include <Windows.h>
 #define BOOST_DATE_TIME_NO_LIB
-#include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time.hpp>
 #include <boost/date_time/local_time/local_time.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 
 using namespace std;
 
 int main()
 {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	setlocale(0, "");
-	uint64_t N;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    setlocale(0, "");
+    uint64_t N;
 
-	cout << "Подготовлено для Softers & Neyton-Devs" << "\n";
-	SetConsoleTextAttribute(hConsole, (WORD)((8 << 4) | 15));
-	system("color F0");
-	cout << "Генератор паролей" << "\n";
-	char Path[16] = "Awl GenPass.txt";
-	string url;  cout << "Адрес сайта: "; cin >> url;
-	string login;  cout << "Логин: "; cin >> login;
-	cout << "Длина пароля: "; cin >> N;
-	srand(time(NULL));
-	char* pass = new char[N + 1];
-	boost::posix_time::ptime utcCur = boost::posix_time::second_clock::local_time();
+    cout << "Подготовлено для Softers & Neyton-Devs" << "\n";
+    SetConsoleTextAttribute(hConsole, (WORD)((8 << 4) | 15));
+    system("color F0");
 
+    cout << "Генератор паролей" << "\n";
+    char Path[16] = "Awl GenPass.txt";
+    string url;  cout << "Адрес сайта: "; cin >> url;
+    string login;  cout << "Логин: "; cin >> login;
+    cout << "Длина пароля: "; cin >> N;
 
-
-	for (uint64_t i = 0; i < N; ++i)
-	{
-		switch (rand() % 3)
-		{
-		case 0:
-			pass[i] = rand() % 32 + '!';
-			break;
-		case 1:
-			pass[i] = rand() % 26 + 'A';
-			break;
-		case 2:
-			pass[i] = rand() % 26 + 'a';
-
-		}
-	}
-
-	pass[N] = 0;
-	cout << pass << "\n";
+    srand(time(NULL));
+    char* password = new char[N + 1];
+    boost::posix_time::ptime utcCur = boost::posix_time::second_clock::local_time();
 
 
-	ofstream out(Path, ios::app);
-	if (out.is_open())
-	{
-		out << utcCur << "\n";
-		out << "Website URL: " << url << "\n";
-		out << "Login: " << login << "\n";
-		out << "Password: " << pass << "\n";
-		out << "\n";
-	}
 
-	out.close();
-	system("pause");
+    for (uint64_t i = 0; i < N; ++i)
+    {
+        switch (rand() % 3)
+        {
+        case 0:
+            password[i] = rand() % 32 + '!';
+            break;
+        case 1:
+            password[i] = rand() % 26 + 'A';
+            break;
+        case 2:
+            password[i] = rand() % 26 + 'a';
+
+        }
+    }
+    cout << "\n";
+    password[N] = 0;
+    cout << password << "\n";
+    ofstream out(Path, ios::app);
+    if (out.is_open())
+    {
+        out << utcCur << "\n";
+        out << "Website URL: " << url << "\n";
+        out << "Login: " << login << "\n";
+        out << "Password: " << password << "\n";
+        out << login << ":" << password << "\n";
+        out << "\n";
+    }
+
+    out.close();
+    cout << "\n";
+    system("pause");
 }

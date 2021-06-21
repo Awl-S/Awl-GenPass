090fb8fa3f4f3a0e56e3e639d84c72fd7a4266a5
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2021-06-21 08:20:13 +0300

    Update
    Update

--- Awl GenPass.cpp
+++ Awl GenPass.cpp
@@ -14,71 +14,143 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-
-#include <iostream>
-#include <fstream>
-#include <ctime> 
-#include <Windows.h>
-#define BOOST_DATE_TIME_NO_LIB
-#include <boost/date_time.hpp>
-#include <boost/date_time/local_time/local_time.hpp>
+#include "libs.h"
 
 using namespace std;
+typedef unsigned int ull;
 
-int main()
-{
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    setlocale(0, "");
-    uint64_t N;
+void data(char* password) {
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+ 
+    int pass_length = strlen(password);
 
-    cout << "Подготовлено для Softers & Neyton-Devs" << "\n";
-    SetConsoleTextAttribute(hConsole, (WORD)((8 << 4) | 15));
-    system("color F0");
+    int point_upper = 0;
+    int point_lower = 0;
+    int point_digit = 0;
+    int point_symbol = 0;
 
-    cout << "Генератор паролей" << "\n";
-    char Path[16] = "Awl GenPass.txt";
-    string url;  cout << "Адрес сайта: "; cin >> url;
-    string login;  cout << "Логин: "; cin >> login;
-    cout << "Длина пароля: "; cin >> N;
+    int point = 0;
 
-    srand(time(NULL));
-    char* password = new char[N + 1];
-    boost::posix_time::ptime utcCur = boost::posix_time::second_clock::local_time();
-
-
-
-    for (uint64_t i = 0; i < N; ++i)
-    {
-        switch (rand() % 3)
-        {
-        case 0:
-            password[i] = rand() % 32 + '!';
-            break;
-        case 1:
-            password[i] = rand() % 26 + 'A';
-            break;
-        case 2:
-            password[i] = rand() % 26 + 'a';
-
-        }
+    for (int i = 0; i < pass_length; i++) {
+      if (password[i] >= 48 && password[i] <= 57) {
+        point_digit++;
+      } else if (password[i] >= 97 && password[i] <= 125) {
+        point_lower++;
+      } else if (password[i] >= 65 && password[i] <= 90) {
+        point_upper++;
+      } else {
+        point_symbol++;
+      }
     }
 
-    cout << "\n";
-    password[N] = 0;
-    cout << password << "\n";
-    ofstream out(Path, ios::app);
-
-    if (out.is_open())
-    {
-        out << utcCur << "\n";
-        out << "Website URL: " << url << "\n";
-        out << "Login: " << login << "\n";
-        out << "Password: " << password << "\n";
-        out << login << ":" << password << "\n";
-        out << "\n";
+    if (point_digit > 0) {
+      point += 50;
     }
 
-    out.close();
-    cout << "\n";
-    system("pause");
+    if (point_lower > 0) {
+      point += 50;
+    }
+
+    if (point_upper > 0) {
+      point += 50;
+    }
+
+    if (point_symbol > 0) {
+      point += 50;
+    }
+
+    cout << "Анализатор пароля: ";
+    if (point_digit > 0 && point_lower > 0 && point_upper > 0 &&
+        point_symbol > 0) {
+      point += 100;
+    }
+
+    if (point <= 50 || pass_length < 8) {
+      SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 4));
+      cout << "~Пароль Слабый" << endl;
+
+    }
+
+    else if (point > 50 && point <= 100) {
+      SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) |15 ));
+
+      cout << "Ну такое" << endl;
+
+    }
+
+    else if (point >= 150 && point <= 200) {
+              SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) |10 ));
+
+      cout << "Сильный" << endl;
+    }
+    
+    else if (point > 200) {
+      SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 2 ));
+      cout << "Вы в безопастности" << endl;
+    }
+  
+}
+
+int main() {
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  setlocale(0, "");
+
+
+  cout << "Подготовлено для Softers & Neyton-Devs" << "\n";
+
+  cout << "Менеджер паролей" << "\n";
+
+  string url;
+  cout << "Адрес сайта: ";
+  cin >> url;
+
+  string login;
+  cout << "Логин: ";
+  cin >> login;
+
+  ull N;
+  cout << "Длина пароля: ";
+  cin >> N;
+
+  srand(time(NULL));
+ 
+  char *password = new char[N + 1];
+  boost::posix_time::ptime utcCur =
+      boost::posix_time::second_clock::local_time();
+
+  for (ull i = 0; i < N; ++i) {
+    switch (rand() % 3) {
+    case 0:
+      password[i] = rand() % 32 + '!';
+      break;
+    case 1:
+      password[i] = rand() % 26 + 'A';
+      break;
+    case 2:
+      password[i] = rand() % 26 + 'a';
+    }
+  }
+
+  cout << "\n";
+  password[N] = 0;
+  cout << password << "\n";
+  ofstream out("AWL-S Password", ios::app);
+
+  if (out.is_open()) {
+    out << utcCur << "\n";
+    out << "Website URL: " << url << "\n";
+    out << "Login: " << login << "\n";
+    out << "Password: " << password << "\n";
+    out << login << ":" << password << "\n";
+    out << "\n";
+  }
+  out.close();
+  data(password);
+
+  delete[] password;
+
+  SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 7));
+
+  cout << "\n";
+  system("pause");
 }

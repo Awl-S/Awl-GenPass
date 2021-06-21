df9b65eb01b9de41bba871997251208a0dea3144
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2021-06-21 08:22:19 +0300

    Add files via upload

---
+++ libs.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <Windows.h>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#define BOOST_DATE_TIME_NO_LIB
+#include <boost/date_time.hpp>
+#include <boost/date_time/local_time/local_time.hpp>

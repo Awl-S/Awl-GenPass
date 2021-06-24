f81468651747f984169757fe89640e2bcc75264d
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2021-06-25 02:59:44 +0300

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,72 +1,80 @@
 #include "libs.h"
 
-void data(char *password) {
-  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+void save(int N, char* password, string url, string login) {
+    cout << "\n";
 
-  int pass_length = strlen(password);
+    if (N <= 100)
+        cout << password << "\n";
+    else
+        cout << "\n" << "А вам оно надо? Пароль сохранился сразу в файл." << "\n";
 
-  int point_upper = 0;
-  int point_lower = 0;
-  int point_digit = 0;
-  int point_symbol = 0;
- 
-  int point = 0;
+    char Path[19] = "AWL-S GenPass.txt";
+    boost::posix_time::ptime localTIME = boost::posix_time::second_clock::local_time();
 
-  for (int i = 0; i < pass_length; i++) {
-    if (password[i] >= 48 && password[i] <= 57) {
-      point_digit++;
-    } else if (password[i] >= 97 && password[i] <= 125) {
-      point_lower++;
-    } else if (password[i] >= 65 && password[i] <= 90) {
-      point_upper++;
-    } else {
-      point_symbol++;
+    ofstream save(Path, ios::app);
+    if (save.is_open()) {
+        save << "\n";
+        save << localTIME << "\n";
+        save << "Website URL: " << url << "\n";
+        save << "Login: " << login << "\n";
+        save << "Password: " << password << "\n";
+        save << login << ":" << password << "\n";
     }
-  }
+    save.close();
 
-  if (point_digit > 0) {
-    point += 50;
-  }
+    cout << "\n";
+};
 
-  if (point_lower > 0) {
-    point += 50;
-  }
+void data(char* password) {
 
-  if (point_upper > 0) {
-    point += 50;
-  }
+    int pass_length = strlen(password), point_upper = 0, point_lower = 0, point_digit = 0, point_symbol = 0, point = 0;
 
-  if (point_symbol > 0) {
-    point += 50;
-  }
+    for (int i = 0; i < pass_length; i++) {
+        if (password[i] >= 48 && password[i] <= 57) 
+            point_digit++;
+        else if (password[i] >= 97 && password[i] <= 125) 
+            point_lower++;
+        else if (password[i] >= 65 && password[i] <= 90) 
+            point_upper++;
+        else 
+            point_symbol++;
+    }
 
-  cout << "Анализатор пароля: ";
-  if (point_digit > 0 && point_lower > 0 && point_upper > 0 &&
-      point_symbol > 0) {
-    point += 100;
-  }
+    if (point_digit > 0)
+        point += 50;
+   
+    if (point_lower > 0) 
+        point += 50;
 
-  if (point <= 50 || pass_length < 8) {
-    SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 4));
-    cout << "~Пароль Слабый" << endl;
+    if (point_upper > 0) 
+        point += 50;
+   
+    if (point_symbol > 0) 
+        point += 50;
 
-  }
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    cout << "Анализатор пароля: ";
+    if (point_digit > 0 && point_lower > 0 && point_upper > 0 &&
+        point_symbol > 0) 
+        point += 100;
 
-  else if (point > 50 && point <= 100) {
-    SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
+    if (point <= 50 || pass_length < 8) {
+        SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 4));
+        cout << "~Пароль Слабый" << endl;
+    }
+    else if (point > 50 && point <= 100) {
+        SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
+        cout << "~Ну такое" << endl;
+    }
+    else if (point >= 150 && point <= 200) {
+        SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 10));
+        cout << "~Сильный" << endl;
+    }
+    else if (point > 200) {
+        SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 2));
+        cout << "~Вы в безопастности" << endl;
+    }
 
-    cout << "~Ну такое" << endl;
-
-  }
-
-  else if (point >= 150 && point <= 200) {
-    SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 10));
-
-    cout << "~Сильный" << endl;
-  }
-
-  else if (point > 200) {
-    SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 2));
-    cout << "~Вы в безопастности" << endl;
-  }
+    delete[] password;
+    SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 7));
 }

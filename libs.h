40f6bdd10922d48cc75b616958df4bbea76b9e0f
Твоя Булочка <85406297+Awl-S@users.noreply.github.com>
2021-06-25 03:00:32 +0300

    Update libs.h

--- libs.h
+++ libs.h
@@ -1,14 +1,16 @@
 #pragma once
-#include <Windows.h>
-#include <ctime>
-#include <fstream>
 #include <iostream>
+#include <string>
+#include <ctime>  
+#include <random> 
+#include <fstream>
+#include <Windows.h>
+
 #define BOOST_DATE_TIME_NO_LIB
 #include <boost/date_time.hpp>
 #include <boost/date_time/local_time/local_time.hpp>
+
 using namespace std;
 
-void data(char *password);
-
-
-
+void save(int N, char* password, string url, string login);
+void data(char* password);
